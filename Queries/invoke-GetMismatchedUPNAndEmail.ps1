1ee372b6cb2c6745547524560326c4a1581d153e
Michael Mardahl <iphase@iphase.dk>
2022-12-09 14:50:04 +0100

    Update invoke-GetMismatchedUPNAndEmail.ps1

--- Queries/invoke-GetMismatchedUPNAndEmail.ps1
+++ Queries/invoke-GetMismatchedUPNAndEmail.ps1
@@ -1,41 +1,39 @@
 <# .DESCRIPTION
-    This script searches Exchange Online mailboxes for users with mismatched UserPrincipalName and PrimarySMTPAddress,
-    and checks whether the user's company attribute matches any of the specified values.
+    This script searches Exchange Online mailboxes for users with mismatched UserPrincipalName and Mail,
+    and outputs the results as a CSV file for further investigation.
 
     .NOTES
     - This script assumes that the Exchange Online PowerShell module is installed and you are connected with enough permissions.
     - This script requires that you have an active Exchange Online subscription, and that you have the necessary
       permissions to access Exchange Online mailboxes.
-    - Before running the script, update the list of allowed company values in the $AllowedCompanies variable to match
-      your desired values.
 
     Author: Michael Mardahl (github.com/mardahl)
     Licensing: MIT
 #>
 
-#Define the list of allowed company values
-$AllowedCompanies = @('Company A', 'CompanyB')
+ # Create an empty array to store the user data
+$UserData = @()
 
-# Search for users with mismatched UserPrincipalName and PrimarySMTPAddress
-$Users = Get-Mailbox -ResultSize Unlimited
+# Populate all users in a sorted array
+$Users = Get-AzureADUser -All $true
+$Users = $Users | Sort-Object DisplayName | Get-Unique -AsString
+
+# Search for users with mismatched UserPrincipalName and Mail
 foreach ($User in $Users) {
-    # Get the user company attribute
-    $recipientDetails = Get-Recipient $User.UserPrincipalName | Select Company
-    if ($AllowedCompanies -contains $recipientDetails.Company) {
-        if ($User.UserPrincipalName -ne $User.PrimarySMTPAddress) {
+    $User.DisplayName
+        if ($User.UserPrincipalName -ne $User.Mail) {
             # Store the user's relevant data in a hash table
             $UserInfo = @{
                 'DisplayName' = $User.DisplayName
                 'UserPrincipalName' = $User.UserPrincipalName
-                'PrimarySMTPAddress' = $User.PrimarySMTPAddress
-                'Company' = $User.Company
-                'IsDirSynced' = $User.IsDirSynced
+                'Mail' = $User.Mail
+                'Company' = $User.CompanyName
+                'DirSyncEnabled' = $User.DirSyncEnabled
             }
         }
         # Add the user data to the array
         $UserData += New-Object PSObject -Property $UserInfo
-    }
 }
 
 # Export the user data to a CSV file
-$UserData | Sort-Object DisplayName | Export-Csv -Path .\MismatchedUserData.csv -NoTypeInformation
+$UserData | Export-Csv -Path .\MismatchedUserDataXXX.csv -NoTypeInformation -Force 
